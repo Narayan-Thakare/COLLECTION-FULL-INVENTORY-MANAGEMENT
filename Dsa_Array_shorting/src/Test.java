b310b4dee5e0581cd04878ccbec33fe8660348c9
Narayan Thakare <narayan111thakare@gmail.com>
2024-02-28 23:36:38 +0530

    first commit

---
+++ Dsa_Array_shorting/src/Test.java
@@ -0,0 +1,39 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a[]={2,5,7,8,9,12};
+		
+		
+		int find=9;
+		
+		for (int i=0; i<a.length ; i++){
+			
+			
+			//System.out.println(a[i]+" "+i );
+			
+			
+if(find==a[i]){
+				
+				
+				System.out.println(i);
+			}
+else{
+	System.out.println("-1");
+}
+			
+	        	}
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
